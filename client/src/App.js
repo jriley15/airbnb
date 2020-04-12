27ba69aabf09213e958fcee5e05dfd356b3a2612
jriley15 <jordanr3@live.com>
2020-04-11 19:11:10 -0700

    apollo querying

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+
+const LISTINGS = gql`
+  query Listing($name: String) {
+    listing(name: $name) {
+      id
+      name
+      listing_url
+      space
+      description
+    }
+  }
+`;
 
 function App() {
+  const [search, setSearch] = useState("");
+  const { loading, error, data, refetch } = useQuery(LISTINGS, {
+    variables: { name: "" },
+  });
+
+  const handleSearchChange = (e) => {
+    refetch({ name: e.target.value });
+    setSearch(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={handleSearchChange}
+        value={search}
+      />
+      {loading ? (
+        <>Loading...</>
+      ) : (
+        data.listing.map((listing, index) => (
+          <pre
+            style={{ wordWrap: "break-word", whiteSpace: "pre-wrap" }}
+            key={index}
+          >
+            {JSON.stringify(listing, null, 2)}
+          </pre>
+        ))
+      )}
     </div>
   );
 }
--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const graphqlHTTP = require("express-graphql");
 const mongo = require("mongoose");
 const app = express();
 const config = require("../config.json");
+const { ApolloServer, gql } = require("apollo-server-express");
+const Listing = require("./listingsAndReviewsSchema");
+var cors = require("cors");
 
 mongo.connect(config.connectionString, {
   useNewUrlParser: true,
@@ -13,12 +15,30 @@ mongo.connection.once("open", async () => {
   console.log("connected to database");
 });
 
-app.use(
-  `/graphiql`,
-  graphqlHTTP({ schema: require(`./schema.js`), graphiql: true })
-);
+const typeDefs = gql`
+  type Query {
+    listing(id: ID, name: String): [Listing]
+  }
+  type Listing {
+    id: ID
+    name: String
+    summary: String
+    listing_url: String
+    space: String
+    description: String
+  }
+`;
 
-app.use("/graphql", graphqlHTTP({ schema: require(`./schema.js`) }));
+const resolvers = {
+  Query: {
+    listing: (parent, args, context, info) => Listing.find(args),
+  },
+};
+
+app.use(cors());
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app });
 
 app.listen(8080, () => {
   console.log("Server running succefully...");
